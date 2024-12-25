99403cc8801ce3c5adf6ff9daa2e4831108988ac
Nagarjuna A J <159552544+cyb3rarjun10@users.noreply.github.com>
2024-12-25 17:48:22 +0530

    first one by owner

---
+++ login.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Pixelbook</title>
+</head>
+
+<style>
+
+	#bar{height:100px;
+		 background-color:black;
+		 text-decoration-color: white;
+		 color:white;
+		 padding:4px;
+		 padding-right: 4px;
+
+		}
+
+	#singup_button{
+		background-color: #0000FF;
+		width:60px;
+		text-align: center;
+		padding:4px;
+		border-radius: 6px;
+		padding-right:10px;
+		text-shadow: black;
+
+	}
+
+	#bar2{
+		  background-color: yellow;
+		  width:800px;
+		  margin:auto;
+		  margin-top:50px;
+		  padding:10px;
+		  padding-top:50px;
+		  text-align: center;
+	}
+
+  
+
+
+</style>
+
+<body style="font-family:tahoma; background-color: #FAD6A5">
+
+	<div id="bar">
+		<div style=" font-size: 40px; padding-left:5px;">Pixelbook</div>
+		<div id="singup_button">Singup</div>
+	</div>
+
+	<div id="bar2">
+        Log in to Pixelbook<br><br>
+        <input type="text" id="text"><br><br>
+        <input type="text" id="text"><br><br>
+        <input type="submit" id="button" value="Log in"><br>
+	</div>
+
+
+	<p> Welcome to my page niggass!</p>
+</body>
+</html>
